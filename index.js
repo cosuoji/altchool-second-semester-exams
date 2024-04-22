bd5f4e79ebb114326a3a45f6bdb0fa7fb992eba2
cosuoji <56638390+cosuoji@users.noreply.github.com>
2024-04-22 17:15:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import blogRoute from "./routes/blogRoute.js";
 
 dotenv.config();
 const app = express(); 
-const PORT = process.env.PORT || 3000
-const MONGODB_URI = process.env.MONGODB_URI || mongodb+srv://test_user:password123456@bookstore.gvhx48w.mongodb.net/?retryWrites=true&w=majority&appName=bookstore
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI 
 app.use(express.json())
 
 
